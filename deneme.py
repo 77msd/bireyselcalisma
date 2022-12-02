1128f598eb252355cc4d4cc95816be3b43154f94
77msd <115848669+77msd@users.noreply.github.com>
2022-12-03 01:58:12 +0300

    Add files via upload

--- deneme.py
+++ deneme.py
@@ -4,9 +4,27 @@
 
 hava="yagmurlu"
 if hava=="yagmurlu":
-    print("montunu giyin")
+    print("montunu giy")
 elif hava=="gunesli":
     print("sorun yok")
+
+
+import math
+print(5*5*5*5)
+print(math.factorial(4))
+
+from turtle import *
+color("red")
+begin_fill()
+pensize(3)
+left(50)
+forward(133)
+circle(50,200)
+right(140)
+circle(50,200)
+forward(133)
+end_fill()
+
 
 
 
